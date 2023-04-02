a2e6413a4184e94daa82c4385c3fd64502ae1de3
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mac-mini.local>
2023-04-02 08:11:22 -0400

    feat(task_repository): implement empty repository
    g

---
+++ flutter_app/packages/task_repository/lib/src/task_repository.dart
@@ -0,0 +1,7 @@
+/// {@template task_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TaskRepository {
+  /// {@macro task_repository}
+  const TaskRepository();
+}
