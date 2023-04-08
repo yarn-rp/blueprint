cc11d98245a01cbe28ed72e631d06ac66f5ac95b
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mac-mini.local>
2023-04-08 16:57:25 -0400

    feat(github_repository): create empty repository

---
+++ flutter_app/packages/platforms_integrations/github_repository/lib/src/github_repository.dart
@@ -0,0 +1,7 @@
+/// {@template github_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class GithubRepository {
+  /// {@macro github_repository}
+  const GithubRepository();
+}
