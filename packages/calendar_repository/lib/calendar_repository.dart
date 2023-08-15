a50e05a891cbe7d651435575039475a7e897f106
Yan Rodriguez <yansarorodriguezpaez@gmail.com>
2023-08-15 17:42:06 -0400

    feat(event-entity): create basic event entity

--- packages/calendar_repository/lib/calendar_repository.dart
+++ packages/calendar_repository/lib/calendar_repository.dart
@@ -1,4 +1,1 @@
-/// A Very Good Project created by Very Good CLI.
-library calendar_repository;
-
 export 'src/calendar_repository.dart';
---
+++ packages/calendar_repository/lib/src/entities/event.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+/// {@template event}
+/// A class that represents an event in the calendar.
+/// {@endtemplate}
+class Event extends Equatable {
+  /// {@macro event}
+  const Event({
+    required this.startTime,
+    required this.endTime,
+    required this.subject,
+    this.isAllDay = false,
+    this.description,
+  });
+
+  /// The start time of the event.
+  final DateTime startTime;
+
+  /// The end time of the event.
+  final DateTime endTime;
+
+  /// The subject of the event.
+  final String subject;
+
+  /// The description of the event.
+  final String? description;
+
+  /// Whether the event is an all day event.
+  final bool isAllDay;
+
+  @override
+  List<Object?> get props => [
+        startTime,
+        endTime,
+        subject,
+        description,
+        isAllDay,
+      ];
+}
