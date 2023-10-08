b53170fdd43825d9c08f9c644ebcb1bbdd49af5e
Yan Rodriguez <61998848+yarn-rp@users.noreply.github.com>
2023-10-08 11:50:23 -0400

    refactor: remove old packages (#30)
    Co-authored-by: Yan Rodriguez <yanrodriguez@Yans-MacBook-Pro.local>

--- packages/flutter_secure_key_pair_storage/lib/flutter_secure_key_pair_storage.dart
+++
@@ -1,3 +0,0 @@
-export 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-export 'src/flutter_secure_key_pair_storage.dart';
--- packages/google_calendar_service/lib/src/entities/entities.dart
+++
@@ -1,2 +0,0 @@
-export 'google_calendar_integration.dart';
-export 'google_calendar_platform.dart';
