819013dee6a170496975eda9e2ba463ec06d1d75
Yan Rodriguez <yanrodriguez@Yans-MacBook-Pro.local>
2023-10-16 14:23:49 -0400

    test(blueprint_repository): add test for extension on classes

---
+++ packages/blueprint_repository/test/src/blueprint_repository_test.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:blueprint_repository/blueprint_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockBlueprintRepository extends Mock
+    implements TodaysBlueprintRepository {}
+
+void main() {
+  group('TodaysBlueprintRepository', () {
+    // can be extended
+    test('can be extended', () {
+      expect(
+        _MockBlueprintRepository(),
+        isA<TodaysBlueprintRepository>(),
+      );
+    });
+  });
+}
