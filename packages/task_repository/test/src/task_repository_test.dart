9be97c92f231e2c2ba2509cb39b44688c04cbf96
Yan Rodriguez <yanrodriguez@Yans-MacBook-Pro.local>
2023-10-16 14:23:25 -0400

    test(task_repository): add test for extension on class

--- packages/task_repository/test/src/task_repository_test.dart
+++ packages/task_repository/test/src/task_repository_test.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:task_repository/src/task_repository.dart';
+
+class _MockTaskRepository extends Mock implements TaskRepository {}
 
 void main() {
   group('TaskRepository', () {
-    test('can be instantiated', () {
-      expect(true, isTrue);
+    test('can be extended', () {
+      expect(
+        _MockTaskRepository(),
+        isA<TaskRepository>(),
+      );
     });
   });
 }
