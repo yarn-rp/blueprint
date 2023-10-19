e406c97280d97290009cbc8b62fcee0d6e4d31a2
DavidZZGi <zzdaviddev@gmail.com>
2023-10-19 17:16:18 -0400

    refactor: reordering an import

--- packages/blueprint_repository/test/src/todays_blueprint_repository_test.dart
+++ packages/blueprint_repository/test/src/todays_blueprint_repository_test.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:blueprint_repository/blueprint_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('TodaysBlueprintRepository', () {
