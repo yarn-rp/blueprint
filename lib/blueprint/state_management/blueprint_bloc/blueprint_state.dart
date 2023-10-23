ed3ab29cc46fe3b832bee2cf64c103b6d68e2f79
Yan Rodriguez <yanrodriguez@Yans-MacBook-Pro.local>
2023-10-23 10:25:06 -0400

    feat(blueprint_state): override equality for blueprint states

--- lib/blueprint/state_management/blueprint_bloc/blueprint_state.dart
+++ lib/blueprint/state_management/blueprint_bloc/blueprint_state.dart
@@ -10,3 +10,47 @@ class BlueprintInitial extends BlueprintState {
   @override
   List<Object?> get props => [];
 }
+
+class BlueprintLoading extends BlueprintState {
+  const BlueprintLoading();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class BlueprintScheduled extends BlueprintState {
+  const BlueprintScheduled({
+    required this.items,
+    required this.events,
+  });
+
+  final List<CalendarEvent> items;
+  final List<GeneralCalendarEvent> events;
+
+  @override
+  List<Object?> get props => [items, events];
+}
+
+class BlueprintNotScheduled extends BlueprintState {
+  const BlueprintNotScheduled({
+    required this.events,
+  });
+
+  final List<GeneralCalendarEvent> events;
+
+  @override
+  List<Object?> get props => [
+        events,
+      ];
+}
+
+class BlueprintError extends BlueprintState {
+  const BlueprintError({
+    required this.error,
+  });
+
+  final String error;
+
+  @override
+  List<Object?> get props => [error];
+}
