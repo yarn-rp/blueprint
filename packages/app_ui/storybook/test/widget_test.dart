c895101c15dc0389db23bfa6590f21ba988ca4d4
Yan Rodriguez <yanrodriguez@Yans-MacBook-Pro.local>
2023-11-02 22:31:14 -0400

    test(widget_test): add dummy test

--- packages/app_ui/storybook/test/widget_test.dart
+++ packages/app_ui/storybook/test/widget_test.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:storybook/main.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+void main() {
+  group('Storybook', () {
+    testWidgets('renders Storybook app', (tester) async {
+      await tester.pumpWidget(const StoryBookApp());
+
+      // expect to find a Storybook widget
+      expect(find.byType(Storybook), findsOneWidget);
+    });
+  });
+}
