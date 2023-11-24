87840b950b1424a1bf4ae2e86145d2abbde66ebc
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2023-11-24 10:10:48 -0500

    feat(user_repository): create new package for user managment

---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,7 @@
+/// {@template user_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class UserRepository {
+  /// {@macro user_repository}
+  const UserRepository();
+}
