f5f7bc1e14b9c212e9ccc83f0540c82adda827aa
DavidZZGi <zzdaviddev@gmail.com>
2023-11-29 11:43:21 -0500

    fix: fixing user repo test file

--- packages/user_repository/test/src/user_repository_test.dart
+++ packages/user_repository/test/src/user_repository_test.dart
@@ -1,7 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:user_repository/user_repository.dart';
+
+class _MockTaskRepository extends Mock implements UserRepository {}
 
 void main() {
-  group('UserRepository', () {});
+  group('UserRepository', () {
+    test('can be extended', () {
+      expect(
+        _MockTaskRepository(),
+        isA<UserRepository>(),
+      );
+    });
+  });
 }
