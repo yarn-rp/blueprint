1b76d78a338b439866417e815e004f1ed3fdd8ee
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2023-12-21 22:10:06 -0500

    refactor(firebase.json): rollback changes

--- lib/blueprint/presentation/widgets/widgets.dart
+++ lib/blueprint/presentation/widgets/widgets.dart
@@ -1,4 +1,4 @@
 export 'calendar_event_tile.dart';
-export 'create_blueprint_dialog.dart';
+export 'create_event_dialog.dart';
 export 'general_calendar_event_tile.dart';
 export 'task_event_tile.dart';
