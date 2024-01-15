d53dfa63f95afa1eabcacd1ef12ddebd0e2dde60
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2024-01-15 10:34:17 -0500

    refactor(available_time_card): localize texts

--- lib/calendar/presentation/widgets/available_time_card.dart
+++ lib/calendar/presentation/widgets/available_time_card.dart
@@ -1,4 +1,5 @@
 import 'package:app_ui/app_ui.dart';
+import 'package:blueprint/core/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
 
@@ -16,6 +17,7 @@ class AvailableTimeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
 
@@ -33,13 +35,13 @@ class AvailableTimeCard extends StatelessWidget {
             Icons.add,
             size: 16,
           ),
-          label: const Text('Add New Task'),
+          label: Text(l10n.addNewTaskCTA),
         ),
       ],
-      labels: const [
+      labels: [
         LabelChip(
-          text: 'Available',
-          avatar: Icon(
+          text: l10n.available,
+          avatar: const Icon(
             Icons.access_time,
             size: 16,
           ),
@@ -47,9 +49,8 @@ class AvailableTimeCard extends StatelessWidget {
       ],
       dateAndTime: '${Jiffy(startTime).jm} - ${Jiffy(endTime).jm}',
       title: EventListTile.calendar(
-        title: 'Free Time',
-        subtitle: 'Seems like you have some time to spare. Feel free to enojoy '
-            'your free time or schedule a new task for yourself',
+        title: l10n.freeTimeEventTitle,
+        subtitle: l10n.freeTimeEventSubtitle,
       ),
     );
   }
