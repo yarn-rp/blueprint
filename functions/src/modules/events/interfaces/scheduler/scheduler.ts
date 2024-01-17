db8149fd58cc9196b9f0f06fb63c8db9627324b6
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2024-01-17 16:28:42 -0500

    refactor(scheduler): change time to 1 minute

--- functions/src/modules/events/interfaces/scheduler/scheduler.ts
+++ functions/src/modules/events/interfaces/scheduler/scheduler.ts
@@ -2,9 +2,13 @@ import { container } from "tsyringe";
 import * as functions from "firebase-functions";
 import { RefreshEventsController } from "../../infrastructure/controllers/refresh-events.controller";
 
+
+// Refers to the time that the scheduler will run
+// https://firebase.google.com/docs/functions/schedule-functions
+const TIME_TO_REFRESH = 1;
 const refresh = container.resolve(RefreshEventsController);
 
-const refreshEvents = functions.pubsub.schedule("every 5 minutes").onRun(async () => {
+const refreshEvents = functions.pubsub.schedule(`every ${TIME_TO_REFRESH} minutes`).onRun(async () => {
   console.log("refreshing events");
   await refresh.execute();
   console.log("tasks refreshed");
